4427fe8a346065777086aebb6de89b480d41006a
Abarcos tech <abarcosltd@gmail.com>
2026-02-15 11:57:28 +0100

    Import Link component in signup.tsx

--- Pages/signup.tsx
+++ Pages/signup.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './signup.css';
 
 export default function Signup() {
