300df334b2e72d1c628a0c86df9b714720e9c2e8
Abarcos tech <abarcosltd@gmail.com>
2026-02-24 13:27:06 +0100

    Fix navigation path to HomeFeed on login success

--- Pages/login.tsx
+++ Pages/login.tsx
@@ -56,7 +56,7 @@ export default function Login() {
       alert('Login successful! Redirecting...');
       
       // Redirect to dashboard or home page
-      navigate('/homefeed');
+      navigate('/HomeFeed');
     })
     .catch(err => {
       // Handle errors
